39344b2337b272514d0e414b5839532d10dec371
linshuling <1611074393@qq.com>
2018-05-05 11:26:03 +0800

    **

---
+++ webpackStudy/3-3webpack编译ES6/demo/webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    entry: {
+        app: './app.js'
+    },
+
+    output: {
+        filename: '[name].[hash:8].js'
+    },
+
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                // use: 'babel-loader',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets : [
+                            ['babel-preset-env',{
+                                targets: {
+                                    browers: ['> 1%','last 2 versions']
+                                    // chrome: '52'
+
+                                }
+                            }]
+                        ]
+                    }
+                },
+                exclude: '/node_modules/'
+            }
+        ]
+    }
+}

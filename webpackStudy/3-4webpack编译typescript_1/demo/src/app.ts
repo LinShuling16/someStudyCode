eb461bd36df83560bdb66e39ef8b64980df1aea8
linshuling <1611074393@qq.com>
2018-05-08 16:32:57 +0800

    *

---
+++ webpackStudy/3-4webpack编译typescript_1/demo/src/app.ts
@@ -0,0 +1,15 @@
+const num = 45;
+ 
+interface Cat {
+    name   : String,
+    gender : String
+}
+
+function touchCat (cat : Cat){
+    console.log('miao!~', cat.name)
+}
+
+touchCat({
+    name : 'tom',
+    gender : 'male'
+})
